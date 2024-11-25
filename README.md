caf422de8d1906e3f0a91cc4c8a2c4e47dbcf458
KippTm <tobimkipp@gmail.com>
2024-11-26 00:02:24 +0100

    Small fixes, plus update in readme

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ Lille program til hjælp med bilag
 ## Usage
     python3 RKasserG.py [type] [file]
 ### Argumenter
-    type - selvskab hvor faktura er fra. (Drinx og carls støttede pt)
+    type - selvskab hvor faktura er fra.
     file - path til faktura filen
+#### Støttede typer
+    Drinx: flag d
+    Carlsberg: flag c
--- RKasserG.py
+++ RKasserG.py
@@ -40,8 +40,8 @@ def check_carls(lines):
 def check_drinx(lines):
     other = ["Pant", "Palle"]
     mixer = ["Maté", "Sprite", "Schweppes"]
-    booze = ["Stoli", "Baileys", "Gammel", "Bacardi", "Fugle", "Cuba", "Jägermeister", "Gin", "Fernet-Branca"]
-    tilbehør = spiritus = vand = energi = pant = 0.0 
+    booze = ["Vodka", "Baileys", "Gammel", "Bacardi", "Fugle", "Cuba", "Jägermeister#", "Gin", "Fernet-Branca"]
+    fustage = cider = tilbehør = spiritus = vand = energi = pant = 0.0 
 
     for l in lines:
         parts = l.split()
@@ -53,12 +53,17 @@ def check_drinx(lines):
             pant += from_danish(parts[len(parts)-1])
         if "Red" in parts and "Bull" in parts:
             energi += from_danish(parts[len(parts)-1])
-        if any(p == "Istønde" for p in parts):
+        if any(p == "Istønde," for p in parts):
             tilbehør += from_danish(parts[len(parts)-1])
+        if "Fustage" in parts and "Pant" not in parts:
+            fustage += from_danish(parts[len(parts)-1])
+        if any(p == "Ice" for p in parts):
+            cider += from_danish(parts[len(parts)-1])
 
-    total = tilbehør + spiritus + vand + energi + pant
+    total = tilbehør + spiritus + vand + energi + pant + fustage + cider
     bilag = [['Tilbehør', tilbehør*1.25],['Spiritus', spiritus*1.25], ['Vand', vand*1.25],
-             ['Energi', energi*1.25], ['Pant', pant*1.25]]
+             ['Energi', energi*1.25], ['Pant', pant*1.25],['Cider', cider*1.25], 
+             ['Fadøl', fustage*1.25],['Total', total*1.25]]
     return bilag
 
 
@@ -73,10 +78,9 @@ def pdf_to_csv(pdfFile, output, arg):
                 case "c":
                     out = check_carls(lines)
                     data += [dat for dat in out]
-                    #data.append(out)
                 case "d":
                     out = check_drinx(lines)
-                    data.append(out)
+                    data += [dat for dat in out]
                 case _:
                     print("Type not supported.")
                     return
@@ -84,11 +88,11 @@ def pdf_to_csv(pdfFile, output, arg):
     frame = pd.DataFrame(data)
     frame.to_csv(output, index=False, header=False) 
 
-place = argv[1]
-file = argv[2]
-output = file.replace(".pdf", ".csv")
 
-if len(argv) < 3 or len(argv) > 3:
+if len(sys.argv) < 3 or len(sys.argv) > 3:
     print("Usage: python3 RKasserG.py [bilag type] [file path]")
 else:
+    place = sys.argv[1]
+    file = sys.argv[2]
+    output = file.replace(".pdf", ".csv")
     pdf_to_csv(file, output, place)
